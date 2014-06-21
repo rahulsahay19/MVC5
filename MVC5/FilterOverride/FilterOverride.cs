af912b61c24c3896f4ad6c216f61c60a83bf27ea
rahul sahay <rahulsahay19@gmail.com>
2014-06-21 19:35:11 +0530

    MVC5 Demo

---
+++ MVC5/FilterOverride/FilterOverride.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace MVC5.FilterOverride
+{
+    public class FilterOverrideAttribute : FilterAttribute, IOverrideFilter
+    {
+        public Type FiltersToOverride
+        {
+            get
+            {
+                return typeof(IAuthorizationFilter);
+            }
+        }
+    }
+}
---
+++ MVC5/Models/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5.Models
+{
+    public class Movies
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string DirectorName { get; set; }
+        public string ReleaseYear { get; set; }
+    }
+}
